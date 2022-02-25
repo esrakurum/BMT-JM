c58cb9046be81b01f677a4cb8998f7bed14185ae
Esra Kurum <50154655+esrakurum@users.noreply.github.com>
2022-02-25 13:58:00 -0800

    Add files via upload

---
+++ jags.R
@@ -0,0 +1,124 @@
+model <- function ()
+{
+  betaX0[1:nt] <- BetaBs[, 1:ncF] %*% Bs.betaX0[1:ncF]
+  betaX1[1:nt] <- BetaBs[, 1:ncF] %*% Bs.betaX1[1:ncF]
+  betaX2[1:nt] <- BetaBs[, 1:ncF] %*% Bs.betaX2[1:ncF]
+  
+  betaZ1[1:nt] <- BetaBs[, 1:ncF] %*% Bs.betaZ1[1:ncF]
+  betaZ2[1:nt] <- BetaBs[, 1:ncF] %*% Bs.betaZ2[1:ncF]
+  
+    
+  for (i in 1:dim_data.id) 
+  {
+  ## Longitudinal Part
+    for(j in (sum(nt.subj[1:i])+1): (sum(nt.subj[1:(i+1)])))
+    {
+      logit(prob[j]) <- betaX0[j - sum(nt.subj[1:i])] + betaX1[j - sum(nt.subj[1:i])] * x1[i] + betaX2[j - sum(nt.subj[1:i])] * x2[i] 
+      + betaZ1[j - sum(nt.subj[1:i])] * z1[i]+ betaZ2[j - sum(nt.subj[1:i])] * z2[i] + b[i] + xi[fac.id[i]]
+      y[j] ~ dbern(prob[j])
+    }  
+    
+  ## Survival part
+    
+    beta.hazardX0[i] <- Bs.betaX0[1:ncF] %*% GammaBs[i, 1:ncF] 
+    beta.hazardX1[i] <- Bs.betaX1[1:ncF] %*% GammaBs[i, 1:ncF]
+    beta.hazardX2[i] <- Bs.betaX2[1:ncF] %*% GammaBs[i, 1:ncF]
+    
+    beta.hazardZ1[i] <- Bs.betaZ1[1:ncF] %*% GammaBs[i, 1:ncF]
+    beta.hazardZ2[i] <- Bs.betaZ2[1:ncF] %*% GammaBs[i, 1:ncF]
+    
+    gamma.hazardX1[i] <- Bs.gammaX1[1:ncF] %*% GammaBs[i, 1:ncF]
+    gamma.hazardX2[i] <- Bs.gammaX2[1:ncF] %*% GammaBs[i, 1:ncF]
+     
+    gamma.hazardZ1[i] <- Bs.gammaZ1[1:ncF] %*% GammaBs[i, 1:ncF]
+    gamma.hazardZ2[i] <- Bs.gammaZ2[1:ncF] %*% GammaBs[i, 1:ncF]
+    
+    prob_hazard[i] <- 1/(1+exp(-(beta.hazardX0[i] + beta.hazardX1[i]*x1[i] + beta.hazardX2[i]*x2[i] + beta.hazardZ1[i]*z1[i] 
+                                + beta.hazardZ2[i]*z2[i] + b[i] + xi[fac.id[i]])))
+    
+    log.h0.T[i] <-  Bs.h0[1:ncF] %*% GammaBs[i, 1:ncF]
+    f.long[i] <- Bs.alpha[1:ncF] %*% GammaBs[i, 1:ncF]
+    
+    haz[i] <- exp(log.h0.T[i] + f.long[i] * prob_hazard[i] + gamma.hazardX1[i]*x2[i] + gamma.hazardX2[i]*x1[i] 
+                  + gamma.hazardZ1[i]*z1[i] + gamma.hazardZ2[i]*z2[i])
+ 
+    for(k in 1:K)
+    {
+      
+       betas.survX0[i, k] <- Bs.betaX0[1:ncF] %*% GammaBs.surv[K * (i - 1) + k, 1:ncF] 
+       betas.survX1[i, k] <- Bs.betaX1[1:ncF] %*% GammaBs.surv[K * (i - 1) + k, 1:ncF]
+       betas.survX2[i, k] <- Bs.betaX2[1:ncF] %*% GammaBs.surv[K * (i - 1) + k, 1:ncF]
+       
+       betas.survZ1[i, k] <- Bs.betaZ1[1:ncF] %*% GammaBs.surv[K * (i - 1) + k, 1:ncF]
+       betas.survZ2[i, k] <- Bs.betaZ2[1:ncF] %*% GammaBs.surv[K * (i - 1) + k, 1:ncF]
+       
+       gamma.survX1[i, k] <- Bs.gammaX1[1:ncF] %*% GammaBs.surv[K * (i - 1) + k, 1:ncF] 
+       gamma.survX2[i, k] <- Bs.gammaX2[1:ncF] %*% GammaBs.surv[K * (i - 1) + k, 1:ncF] 
+       
+       gamma.survZ1[i, k] <- Bs.gammaZ1[1:ncF] %*% GammaBs.surv[K * (i - 1) + k, 1:ncF] 
+       gamma.survZ2[i, k] <- Bs.gammaZ2[1:ncF] %*% GammaBs.surv[K * (i - 1) + k, 1:ncF] 
+       
+       prob_surv[i, k] <- 1/(1+exp(-(betas.survX0[i, k] + betas.survX1[i, k]*x1[i] + betas.survX2[i, k]*x2[i] 
+                                     + betas.survZ1[i, k]*z1[i] + betas.survZ2[i, k]*z2[i] + b[i] + xi[fac.id[i]])))
+       
+       log.h0.s[i, k] <- Bs.h0[1:ncF] %*% GammaBs.surv[K * (i - 1) + k, 1:ncF]
+       f.long.surv[i, k] <- Bs.alpha[1:ncF]%*% GammaBs.surv[K * (i - 1) + k, 1:ncF]
+       
+       surv[i, k] <- exp(log.h0.s[i, k] + f.long.surv[i, k] * prob_surv[i, k]  + gamma.survX1[i, k]*x2[i] + gamma.survX2[i, k]*x1[i] 
+                         + gamma.survZ1[i, k]*z1[i] + gamma.survZ2[i, k]*z2[i])
+           }
+    log.survival[i] <-  -T1[i]/2*inprod(wk, surv[i ,])  
+    phi[i] <- 100000 - (event[i] * log(haz[i])) - log.survival[i] 
+    zeros[i] ~ dpois(phi[i]) 
+  }
+    
+  # Priors
+  for (l in 1:dim_data.id)
+  {
+    b[l] ~ dnorm(mu0, 1/sigmasq_S)
+  }
+  for(k in 1:n)
+  {
+    xi[k] ~ dnorm(mu1, 1/sigmasq_F)
+  }
+  
+  
+  Bs.betaX0[1:ncF] ~ dmnorm(priorMean.Bs.betaX0[], Tau.betaX0 * priorTau.betaX0[, ])
+  Bs.betaX1[1:ncF] ~ dmnorm(priorMean.Bs.betaX1[], Tau.betaX1 * priorTau.betaX1[, ])
+  Bs.betaX2[1:ncF] ~ dmnorm(priorMean.Bs.betaX2[], Tau.betaX2 * priorTau.betaX2[, ])
+
+  Bs.betaZ1[1:ncF] ~ dmnorm(priorMean.Bs.betaZ1[], Tau.betaZ1 * priorTau.betaZ1[, ])
+  Bs.betaZ2[1:ncF] ~ dmnorm(priorMean.Bs.betaZ2[], Tau.betaZ2 * priorTau.betaZ2[, ])
+  
+
+  Tau.betaX0 ~ dgamma(1, 0.005)
+  Tau.betaX1 ~ dgamma(1, 0.005)
+  Tau.betaX2 ~ dgamma(1, 0.005)
+  
+  Tau.betaZ1 ~ dgamma(1, 0.005)
+  Tau.betaZ2 ~ dgamma(1, 0.005)
+  
+  sigmasq_S ~ dgamma(priorA, priorB)
+  
+  sigmasq_F ~ dgamma(priorA, priorB)
+  
+  Bs.gammaX1[1:ncF] ~ dmnorm(priorMean.Bs.gammaX1[], Tau.gammaX1 * priorTau.gammaX1[, ])
+  Bs.gammaX2[1:ncF] ~ dmnorm(priorMean.Bs.gammaX2[], Tau.gammaX2 * priorTau.gammaX2[, ])
+   
+  Tau.gammaX1 ~ dgamma(1, 0.005)
+  Tau.gammaX2 ~ dgamma(1, 0.005)
+   
+  Bs.gammaZ1[1:ncF] ~ dmnorm(priorMean.Bs.gammaZ1[], Tau.gammaZ1 * priorTau.gammaZ1[, ])
+  Bs.gammaZ2[1:ncF] ~ dmnorm(priorMean.Bs.gammaZ2[], Tau.gammaZ2 * priorTau.gammaZ2[, ])
+   
+  Tau.gammaZ1 ~ dgamma(1, 0.005)
+  Tau.gammaZ2 ~ dgamma(1, 0.005)
+  
+  Bs.alpha[1:ncF] ~ dmnorm(priorMean.Bs.alpha[],  Tau.alphas * priorTau.alpha[, ])
+  
+  Tau.alphas ~ dgamma(0.01, 0.5) ##
+  
+  Bs.h0[1:ncF] ~ dmnorm(priorMean.Bs.h0[], Tau.Bs.h0 * priorTau.Bs.h0[,])
+  Tau.Bs.h0 ~ dgamma(0.1, 0.005)
+  
+}
---
+++ prepare_data.R
@@ -0,0 +1,200 @@
+
+
+##Number of scheduled observations within each subject
+nt <- 20
+gridPoints <- seq(0, 1, length.out = nt)
+
+
+nknots <- 20
+
+
+##Number of facilities:
+n <- length(unique(dat$facility_id))
+
+
+## Time-invariant data, one row per subject:
+data.id <- dat[!duplicated(dat$subject_id), ]
+
+##Number of subjects wit.hin each facility:
+nf <- table(x = data.id$facility_id)
+
+n.subj <- c(as.vector(nf))
+
+
+Time.e <- data.id$time_surv
+
+
+## 15-point Gauss-Kronrod rule for the integrals wit.hin the survival model
+
+wk <- c(0.0630920926299786, 0.140653259715526, 0.190350578064785, 
+                0.209482141084728, 0.190350578064785, 0.140653259715526, 
+                0.0630920926299786, 0.0229353220105292, 0.10479001032225, 
+                0.169004726639268, 0.204432940075299, 0.204432940075299, 
+                0.169004726639268, 0.10479001032225, 0.0229353220105292)
+sk <- c(-0.949107912342758, -0.741531185599394, -0.405845151377397, 
+        0, 0.405845151377397, 0.741531185599394, 0.949107912342758, 
+        -0.991455371120813, -0.864864423359769, -0.586087235467691, 
+        -0.207784955007898, 0.207784955007898, 0.586087235467691, 
+        0.864864423359769, 0.991455371120813)
+
+ordsk <- order(sk)
+sk <- sk[ordsk]
+wk <- wk[ordsk]
+
+K <- length(sk)
+
+P <- Time.e/2
+st <- outer(P, sk + 1) 
+
+############################################
+# Functions for P-splines
+
+# truncated p-th power function
+tpower <- function(x, t, p)
+  (x - t) ^ p * (x > t)
+
+# B-spline basis with degree deg
+bbase1 <- function(x, xl, xr, ndx, deg){
+  dx <- (xr - xl) / ndx
+  knots <- seq(xl - deg * dx, xr + deg * dx, by = dx)
+  P <- outer(x, knots, tpower, deg)
+  n <- dim(P)[2]
+  D <- diff(diag(n), diff = deg + 1) / (gamma(deg + 1) * dx ^ deg)
+  B <- (-1) ^ (deg + 1) * P %*% t(D)
+  B 
+}
+
+############################################
+# P-splines for the coefficients in the longitudinal model 
+
+t.hi = floor(max(Time.e)) +1
+t.lo = 0
+n.seg = nknots - 2
+deg = 2
+
+
+Betabs = bbase1(gridPoints, t.lo, t.hi, n.seg, deg)
+BetaBs <- Betabs
+
+Dbeta <- diag(ncol(BetaBs))
+priorTau.betaX0 <- crossprod(diff(Dbeta, diff = 2)) + 1e-06 * Dbeta
+priorTau.betaX1 <- crossprod(diff(Dbeta, diff = 2)) + 1e-06 * Dbeta
+priorTau.betaX2 <- crossprod(diff(Dbeta, diff = 2)) + 1e-06 * Dbeta
+
+
+priorTau.betaZ1 <- crossprod(diff(Dbeta, diff = 2)) + 1e-06 * Dbeta
+priorTau.betaZ2 <- crossprod(diff(Dbeta, diff = 2)) + 1e-06 * Dbeta
+
+############################################
+# P-splines for the coefficients in the survival model 
+
+t.hi = floor(max(Time.e)) +1
+t.lo = 0
+n.seg = nknots-2
+deg = 2
+
+Gammabs = bbase1(Time.e, t.lo, t.hi, n.seg, deg)
+GammaBs <- Gammabs
+
+Gammas = bbase1(c(t(st)), t.lo, t.hi, n.seg, deg)
+GammaBs.surv <- Gammas
+
+
+Dgamma <- diag(ncol(GammaBs))
+
+priorTau.Bs.h0 <- crossprod(diff(Dgamma, diff = 2)) + 1e-06 * Dgamma
+
+priorTau.gammaX1 <- crossprod(diff(Dgamma, diff = 2)) + 1e-06 * Dgamma
+
+priorTau.gammaX2 <- crossprod(diff(Dgamma, diff = 2)) + 1e-06 * Dgamma
+
+priorTau.gammaZ1 <- crossprod(diff(Dgamma, diff = 2)) + 1e-06 * Dgamma
+
+priorTau.gammaZ2 <- crossprod(diff(Dgamma, diff = 2)) + 1e-06 * Dgamma
+
+priorTau.alphas <- crossprod(diff(Dgamma, diff = 2)) + 1e-06 * Dgamma
+
+############################################
+
+
+###Initials:
+
+Bs.betaX0 <- rep(0, nknots)
+
+Bs.betaX1 <- rep(0, nknots)
+
+Bs.betaX2 <- rep(0, nknots)
+
+Bs.betaZ1 <- rep(0, nknots)
+
+Bs.betaZ2 <- rep(0, nknots)
+
+
+Bs.h0 <- rep(0, nknots)
+
+Bs.gammaX1 <- rep(0, nknots)
+Bs.gammaX2 <- rep(0, nknots)
+
+Bs.gammaZ1 <- rep(0, nknots)
+Bs.gammaZ2 <- rep(0, nknots)
+
+Bs.alpha <- rep(0, nknots)
+
+
+############################################
+# Data for the MCMC
+
+Data <- list(n = n, 
+             K = K,
+             event = data.id$survival, 
+             T1 = data.id$time_surv,
+             y = dat$y, 
+             x1 = data.id$x1,
+             x2 = data.id$x2,
+             z1 = data.id$z1,
+             z2 = data.id$z2,
+             zeros = rep(0, dim(data.id)[1]),
+             wk = wk,
+             GammaBs.surv = GammaBs.surv,
+             GammaBs = GammaBs,
+             priorMean.Bs.betaX0 = Bs.betaX0,
+             priorTau.betaX0 = priorTau.betaX0,
+             BetaBs = BetaBs,
+             priorMean.Bs.betaX1 = Bs.betaX1,
+             priorTau.betaX1 = priorTau.betaX1,
+             priorMean.Bs.betaX2 = Bs.betaX2,
+             priorTau.betaX2 = priorTau.betaX2,
+             priorMean.Bs.betaZ1 = Bs.betaZ1,
+             priorTau.betaZ1 = priorTau.betaZ1,
+             priorMean.Bs.betaZ2 = Bs.betaZ2,
+             priorTau.betaZ2 = priorTau.betaZ2,
+             priorA = 2,
+             priorB = 0.5,
+             mu0 = 0,
+             mu1 = 0,
+             priorMean.Bs.alpha = Bs.alpha,
+             priorTau.alpha = priorTau.alphas, 
+             priorMean.Bs.gammaX1 = Bs.gammaX1,
+             priorTau.gammaX1 = priorTau.gammaX1, 
+             priorMean.Bs.gammaX2 = Bs.gammaX2,
+             priorTau.gammaX2 = priorTau.gammaX2, 
+             priorMean.Bs.gammaZ1 = Bs.gammaZ1,
+             priorTau.gammaZ1 = priorTau.gammaZ1, 
+             priorMean.Bs.gammaZ2 = Bs.gammaZ2,
+             priorTau.gammaZ2 = priorTau.gammaZ2, 
+             priorMean.Bs.h0 = Bs.h0,
+             priorTau.Bs.h0 = priorTau.Bs.h0,
+             ncF = nknots,
+             nt = nt,
+             nt.subj = c(0, rep(20, dim(data.id)[1])),
+             fac.id = rep(1:n, n.subj),
+             dim_data.id = dim(data.id)[1]
+)
+
+############################################
+
+
+### Parameters to keep track of during the MCMC,
+### Bs.betas0, Bs.betas1, Bs.betas2
+parms <- c( "Bs.betaX0", "Bs.betaX1", "Bs.betaX2", "Bs.betaZ1",  "Bs.betaZ2", "Bs.alpha", "Bs.h0", "Bs.gammaX1", "Bs.gammaX2", "Bs.gammaZ1", "Bs.gammaZ2", "sigmasq_S", "sigmasq_F")
+
